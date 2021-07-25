f7d5495c9d6f817a8558433abe6c883a89e1a0de
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-07-25 12:56:47 -0500

    Build basic movie and keyword model/schema

--- backend/search_engine/models.py
+++ backend/search_engine/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    tagline = models.CharField(max_length=500)
+    overview = models.CharField(max_length=2000)
+    vote_average = models.DecimalField(max_digits=3, decimal_places=1)
+
+    keywords = models.ManyToManyField('Keyword', related_name='movies')
+
+    def __str__(self):
+        return self.title
+
+class Keyword(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
