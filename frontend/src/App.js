994d0e9467ff5d4e572e68da26f3ef699e3908b1
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-07-31 01:15:41 -0500

    Create react frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div>
+            <h1>Hello World</h1>
+          </div>
+        </div>
+      </div>
+    ); 
+  }
+}
+
+export default App;
