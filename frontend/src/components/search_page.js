6e5ce6dbd90bdd372c6dd0260a98cd776e33a592
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-07-31 19:34:57 -0500

    Add page navigation button group

--- frontend/src/components/search_page.js
+++ frontend/src/components/search_page.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import SearchForm from './search_form'
 import axios from 'axios'
-import SearchResult from './search_result';
+import SearchResult from './search_result'
+import PageNavigator from './page_navigator'
 
 class SearchPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      search_results: []
+      searchResults: [],
+      nextPageUrl: null,
+      prevPageUrl: null,
     }
   }
 
-  componentDidMount() {
-    this.searchMovies('', '', '', '');
-  }
-
-  searchMovies = (title, keywords, vote_lower_bound, vote_upper_bound) => {
+  searchAndUpdateMovies = (title, keywords, vote_lower_bound, vote_upper_bound) => {
     let params = {}
     // Empty inputs shouldn't be added to query string
     if (title !== '') params.title = title
@@ -23,15 +22,30 @@ class SearchPage extends React.Component {
     if (vote_lower_bound !== '') params.vote_lower_bound = vote_lower_bound
     if (vote_upper_bound !== '') params.vote_lower_bound = vote_upper_bound
 
+    // Send GET request and update state based on request
     axios
       .get('http://localhost:8000/api/searchengine/search/', { params: params })
-      // .then((res) => console.log([res.data.results]))
-      .then((res) => this.setState({ search_results: res.data.results }))
+      .then((res) => this.setStateBySearchResponse(res.data))
       .catch((err) => console.log(err))
   }
 
+  // Sends GET request by URL. Useful for pagination by links
+  searchAndUpdateMoviesUrl = (url) => {
+    axios
+      .get(url)
+      .then((res) => this.setState(this.setStateBySearchResponse(res.data)))
+  }
+
+  setStateBySearchResponse = (response) => {
+    this.setState({
+      searchResults: response.results,
+      nextPageUrl: response.next,
+      prevPageUrl: response.previous,
+    })
+  }
+
   renderSearchResults = () => {
-    return this.state.search_results.map((result) => (
+    return this.state.searchResults.map((result) => (
       <li
         key={result.id}
       >
@@ -47,11 +61,16 @@ class SearchPage extends React.Component {
       <div>
         <h1>Movie Search Engine</h1>
         <SearchForm
-          onSubmit={this.searchMovies}
+          onSubmit={this.searchAndUpdateMovies}
         />
         <ul>
           {this.renderSearchResults()}
         </ul>
+        <PageNavigator
+          nextPageUrl={this.state.nextPageUrl}
+          prevPageUrl={this.state.prevPageUrl}
+          onChangePage={this.searchAndUpdateMoviesUrl}
+        />
       </div>
     );
   }
